80e65e1dc6f768f7b51a19dddb7aa36bb20accb4
Jorge <horuhe90@gmail.com>
2016-03-01 11:31:55 -0600

    Actualización de un contacto

---
+++ app/src/main/java/gpovallas/obj/Contact.java
@@ -0,0 +1,19 @@
+package gpovallas.obj;
+
+public class Contact {
+    public Contact() {
+    }
+
+    public String pk_contacto_cliente;
+    public String fk_cliente;
+    public String fk_pais;
+    public String nombre;
+    public String apellidos;
+    public String titulo;
+    public String cargo;
+    public String telefono;
+    public String celular;
+    public String email;
+    public Integer estado;
+    public String token;
+}
